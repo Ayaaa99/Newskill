eb6c3b7020453fce7852368fd9a081307ec45fe3
Ayaaa99 <chz011@ucsd.edu>
2022-05-01 22:31:00 -0700

    changes

--- JunitTest.java
+++ JunitTest.java
@@ -1,6 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+import org.junit.Assert;
 public class JunitTest {
     @Test
     public void subtraction(){
-        assertEqual(1, Javafile.minus(3));
+        assertEquals(1, Javafile.minus(3));
     }
 }
